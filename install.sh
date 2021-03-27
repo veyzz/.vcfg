658195beb1d131aeda7121a156f56fc65e2c6c22
veyzz <munir.taga@gmail.com>
2021-03-27 11:08:24 +0300

    customize vim for python

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd
-mkdir -p ~/.config/ranger
+mkdir -p ~/.vim/ftplugin
 
 ln -sft ~ ~/.vcfg/.bashrc
 ln -sft ~ ~/.vcfg/.vimrc
 ln -sft ~ ~/.vcfg/.tmux.conf
 ln -sft ~ ~/.vcfg/.tmux.conf.local
+ln -sft ~/.vim/ftplugin ~/.vcfg/.vim/ftplugin/python.vim
 
