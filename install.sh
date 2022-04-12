a8af29929c18ded50d7d1eb0a0af4da913c1fda3
veyzz <munir.taga@gmail.com>
2022-04-12 11:42:46 +0300

    add git alias

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -sft ~ ~/.vcfg/.tmux.conf
 ln -sft ~ ~/.vcfg/.tmux.conf.local
 ln -sft ~/.vim/ftplugin ~/.vcfg/.vim/ftplugin/python.vim
 
+# git config modifications
+git config --global alias.lg "log --graph --date-order --abbrev-commit --date=relative \
+  --pretty=format:'%C(auto)%h -%d%C(reset) %s %C(cyan)(%cr) %C(bold black)<%an>%C(reset)'"
+
