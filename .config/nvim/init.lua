61bff7261e7217ab615b5de0b8b5cd6ddc2c654c
veyzz <munir.taga@gmail.com>
2025-04-13 20:01:38 +0300

    neovim: add fzf-plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,7 +45,7 @@ require('lazy').setup({
   {'roxma/vim-tmux-clipboard'},
   {'christoomey/vim-tmux-navigator'},
   {'tpope/vim-fugitive'},
-  {'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
+  {'junegunn/fzf.vim', dependencies = {'junegunn/fzf'}}
 })
 
 -- Main theme
@@ -256,14 +256,47 @@ vim.keymap.set('n', '<Leader>k', '<Plug>(easymotion-k)')
 vim.keymap.set('n', '<Leader>h', '<Plug>(easymotion-linebackward)')
 vim.keymap.set('n', '<Leader>l', '<Plug>(easymotion-lineforward)')
 
--- Telescope (замена FZF)
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<Leader>]', function()
-  require('telescope.builtin').grep_string({
-    additional_args = '--ignore=tags --ignore=cscope.out'
-  })
-end)
-vim.keymap.set('n', '<Leader>g]', telescope.current_buffer_tags)
+-- Fuzzy search and preview
+vim.env.FZF_DEFAULT_OPTS = '--bind ' .. table.concat({
+  'ctrl-y:preview-up',
+  'ctrl-e:preview-down',
+  'ctrl-b:preview-page-up',
+  'ctrl-f:preview-page-down',
+  'ctrl-u:preview-half-page-up',
+  'ctrl-d:preview-half-page-down',
+  'shift-up:preview-top',
+  'shift-down:preview-bottom'
+}, ',')
+
+local function Ag(opts)
+  local args = opts.args
+  local fzf_options = {
+    '--ignore=tags',
+    '--ignore=cscope.out',
+    '--skip-vcs-ignores',
+    '--hidden'
+  }
+
+  vim.fn['fzf#vim#ag'](args, table.concat(fzf_options, ' '),
+                       vim.fn['fzf#vim#with_preview'](),
+                       opts.bang and 1 or 0)
+end
+
+local function ag_cword()
+  local cword = vim.fn.expand('<cword>')
+  vim.cmd('Ag ' .. cword)
+end
+
+vim.api.nvim_create_user_command('Ag', Ag, {bang = true,
+                                            nargs = '*',
+                                            desc = 'Fuzzy search with AG'})
+
+vim.keymap.set('n', '<Leader>]', ag_cword, {silent = true,
+                                            noremap = true,
+                                            desc = 'Search current word'})
+vim.keymap.set('n', '<Leader>g]', ':BTags<CR>', {silent = true,
+                                                 noremap = true,
+                                                 desc = 'Search buffer tags'})
 
 -- Toggle keeping cursor in the middle all the time
 local center_mode = false
