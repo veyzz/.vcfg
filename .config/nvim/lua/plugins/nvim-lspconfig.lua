504fb6e7091cbd2763c064c734b9465d30184224
veyzz <munir.taga@gmail.com>
2025-12-29 14:49:04 +0300

    neovim: fix LSP config

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,5 @@
 vim.diagnostic.config({ virtual_text = true })
 
-require('lspconfig').pylsp.setup({
-  autostart = false,
-})
-
-require('lspconfig').clangd.setup({
-  autostart = false,
-  on_attach = function(client, bufnr)
-    vim.bo[bufnr].tagfunc = nil
-  end
-})
-
 vim.api.nvim_create_user_command('Symbols', vim.lsp.buf.document_symbol,
                                  {desc = 'Show all symbols in file'})
 
@@ -18,8 +7,10 @@ vim.keymap.set('n', '<Leader>d', function()
   if not vim.lsp.buf_is_attached() then
     --vim.cmd.LspStart()
     vim.lsp.enable('clangd')
-    vim.lsp.enable('pylsp')
+    --vim.lsp.enable('pylsp')
   else
-    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+    vim.lsp.enable('clangd', false)
+    --vim.lsp.enable('pylsp', false)
+    --vim.diagnostic.enable(not vim.diagnostic.is_enabled())
   end
 end, {desc = 'Start LSP on first running and toggle diagnostic'})
