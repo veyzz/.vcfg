d3ddc9ddab97555b5e08a86a98794e6eab2f6759
veyzz <munir.taga@gmail.com>
2026-02-28 21:03:05 +0300

    neovim: add command to toggle colorscheme

--- .config/nvim/lua/core/commands.lua
+++ .config/nvim/lua/core/commands.lua
@@ -1,1 +1,17 @@
 vim.api.nvim_create_user_command('Glow', 'vert term glow %', {})
+
+vim.api.nvim_create_user_command('ToggleColorscheme', function()
+    if vim.opt.termguicolors:get() then
+        vim.opt.termguicolors = false
+        pcall(vim.cmd.colorscheme, "default")
+    else
+        vim.opt.termguicolors = true
+
+        local colors = require('core.colors')
+        require('monokai').setup({
+            palette = colors.palette,
+            custom_hlgroups = colors.hlgroups,
+            italics = false,
+        })
+    end
+end, {})
